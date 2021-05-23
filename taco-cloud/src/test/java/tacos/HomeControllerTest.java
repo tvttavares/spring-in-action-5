f4ad93cd1d8d0c1fa96f6eb2beab0d5088cfced8
Thiago Tavares <tvttavares@gmail.com>
2021-05-23 11:47:11 -0400

    Foundational Spring

---
+++ taco-cloud/src/test/java/tacos/HomeControllerTest.java
@@ -0,0 +1,33 @@
+package tacos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomeController.class)   // <1>
+public class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;   // <2>
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))    // <3>
+
+                .andExpect(status().isOk())  // <4>
+
+                .andExpect(view().name("home"))  // <5>
+
+                .andExpect(content().string(           // <6>
+                        containsString("Welcome to...")));
+    }
+
+}
